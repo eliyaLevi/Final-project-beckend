2168a230b72a29600c68f79b6087186fc9a07f5d
eliyaLevi <el0547135208@gmail.com>
2024-12-24 16:40:44 +0200

    cors

--- index.ts
+++ index.ts
@@ -24,7 +24,7 @@ const io = new Server(server, {
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://final-project-beckend.onrender.com",
     credentials: true,
   })
 );
